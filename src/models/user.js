44147d25a13ed307752e0306ec747261c9c38a30
sayantanroy2019 <89955820+sayantanroy2019@users.noreply.github.com>
2026-05-19 21:08:43 +0530

    validation and sanitisation

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
     {
@@ -19,7 +20,12 @@ const userSchema = new mongoose.Schema(
             required: true,
             unique: true,
             lowercase: true,
-            trim: true
+            trim: true,
+            validate(value){
+                if(!validator.isEmail(value)){
+                    throw new Error("Invalid email address: " + value);
+                }
+            }
         }
         , password:{
             type: String,
